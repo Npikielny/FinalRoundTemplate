400a47d2b9d6f0b80b274fc977209c1c69d8ef5a
Sergio Diaz <spd75@cornell.edu>
2022-02-11 16:43:10 -0500

    Final Price

--- SwiftUIDiftUI/CartView.swift
+++ SwiftUIDiftUI/CartView.swift
@@ -15,17 +15,35 @@ struct CartView: View {
             .enumerated()
             .filter { $0.element.inCart }
             .map { $0.offset }
-        List {
-            ForEach(cartItemIndices, id: \.self) { idx in
-                CartItemRow(item: _items[idx])
-            }
-            .onDelete { idxSet in
-                idxSet.forEach {
-                    items[cartItemIndices[$0]].inCart.toggle()
+
+        let price = items
+            .filter { $0.inCart }
+            .map { $0.price }
+            .reduce(0, +)
+
+
+        VStack {
+            HStack {
+                Text("Total Price")
+                    .font(.headline)
+                Spacer()
+                Text("$\(price, specifier: "%.2f")")
+                    .font(.headline)
+            }.padding(12)
+
+            List {
+                ForEach(cartItemIndices, id: \.self) { idx in
+                    CartItemRow(item: _items[idx])
+                }
+                .onDelete { idxSet in
+                    idxSet.forEach {
+                        items[cartItemIndices[$0]].inCart.toggle()
+                    }
                 }
             }
         }
     }
+
 }
 
 struct CartView_Previews: PreviewProvider {
